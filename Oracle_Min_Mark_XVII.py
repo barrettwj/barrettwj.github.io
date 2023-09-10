479a440d3dd34f7d6e9931078b613803845a89d4
barrettwj <waylonbarrett6@gmail.com>
2023-09-09 23:04:40 -0500

    Add files via upload

--- Oracle_Min_Mark_XVII.py
+++ Oracle_Min_Mark_XVII.py
@@ -1,12 +1,14 @@
 import random
+import math
 class Oracle:
     def __init__(self):
         self.H = 6#--------------------------------------------------------------------------------------------------------------HP
-        self.K = 41#-------------------------------------------------------------------------------------------------------------HP
+        self.K = 39#-------------------------------------------------------------------------------------------------------------HP
         self.M = 49#-------------------------------------------------------------------------------------------------------------HP
-        self.PV = 87#------ -----------------------------------------------------------------------------------------------------HP
-        self.C = 2000#-----------------------------------------------------------------------------------------------------------HP
         self.N = (self.K * self.M)
+        self.PV = 77#------------------------------------------------------------------------------------------------------------HP
+        max_pct = 1.0#-----------------------------------------------------------------------------------------------------------HP
+        self.C = math.floor(float(self.N) * max_pct)
         self.m = [Matrix(self, a) for a in range(self.H)]
         #__________________________________________________________________________________________________________________________
         ts_dim = 5#--------------------------------------------------------------------------------------------------------------HP
@@ -33,42 +35,47 @@ class Matrix:
         self.mi = mi_in
         self.ffi = (self.mi - 1)
         self.fbi = ((self.mi + 1) % self.po.H)
-        self.e = dict()
-        self.ov = self.av = set()
+        # self.e = dict()
+        self.e = {a:dict() for a in range(self.po.N)}
+        self.ov = self.av = self.conf = set()
         self.ppcL = self.ppcR = -1
-        self.agency = False
     def update(self):
         #__________________________________________________________________________________________________________________________
-        # fbv = self.po.m[self.fbi].av.copy() if (self.fbi != 0) else set()
-        fbv = self.po.m[self.fbi].av.copy()
+        # fbv = self.po.m[self.fbi].conf.copy() if (self.fbi != 0) else set()
+        fbv = self.po.m[self.fbi].conf.copy()
         cv = (self.av | {(a + self.po.N) for a in fbv})
         avs = {a:len(set(self.e[a].keys()) ^ cv) for a in self.e.keys()}
         self.av = {a for a in avs.keys() if (avs[a] == min(avs.values()))}
-        conf = set()
+        self.conf = set()
         for a in self.av:
             cli = (a // self.po.M)
-            if (len(self.av & set(range((self.po.M * cli), (self.po.M * (cli + 1))))) == 1): conf.add(a)
+            if (len(self.av & set(range((self.po.M * cli), (self.po.M * (cli + 1))))) == 1): self.conf.add(cli)
         if (self.mi == 0):
             #______________________________________________________________________________________________________________________
             bv = 0
-            if (self.ppcL in self.av):
-                if (len(self.av & set(range((self.po.M * (self.ppcL // self.po.M)),
-                                            (self.po.M * ((self.ppcL // self.po.M) + 1))))) == 1): bv += -1
-            if (self.ppcR in self.av):
-                if (len(self.av & set(range((self.po.M * (self.ppcR // self.po.M)),
-                                            (self.po.M * ((self.ppcR // self.po.M) + 1))))) == 1): bv += 1
+            if (self.ppcL in self.conf): bv += -1
+            if (self.ppcR in self.conf): bv += 1
             self.agency = (bv != 0)
             if (bv == 0):
                 if ((self.ppcL == -1) and (self.ppcR != -1)): bv = -1
                 if ((self.ppcR == -1) and (self.ppcL != -1)): bv = 1
                 if ((self.ppcL == -1) and (self.ppcR == -1)): bv = random.choice([-1, 1])
-                if (bv == 0): bv = random.choice([-1, 1])
+                if ((bv == 0) and (random.randrange(1000000) < 100000)): bv = random.choice([-1, 1])#-synapse discovery----------HP
                 if (bv == -1):
-                    tv = (self.av - {self.ppcR})
+                    tv = (self.conf - {self.ppcR})
                     self.ppcL = random.choice(list(tv)) if (len(tv) > 0) else -1
                 if (bv == 1):
-                    tv = (self.av - {self.ppcL})
+                    tv = (self.conf - {self.ppcL})
                     self.ppcR = random.choice(list(tv)) if (len(tv) > 0) else -1
+            else:
+                if (self.ppcL in self.conf):
+                    ci = set(range((self.po.M * self.ppcL), (self.po.M * (self.ppcL + 1))))
+                    b = random.choice(list(ci))
+                    for a in cv: self.e[b][a] = self.po.PV
+                if (self.ppcR in self.conf):
+                    ci = set(range((self.po.M * self.ppcR), (self.po.M * (self.ppcR + 1))))
+                    b = random.choice(list(ci))
+                    for a in cv: self.e[b][a] = self.po.PV
             #_____________________________________________________________________________________________________________________
             self.po.ts_index = ((self.po.ts_index + len(self.po.ts) + bv) % len(self.po.ts))
             iv = self.po.ts[self.po.ts_index].copy()
@@ -76,6 +83,7 @@ class Matrix:
         #_________________________________________________________________________________________________________________________
         exp = set()
         self.ov = set()
+        emr = len(self.conf ^ iv)
         em = zr = mr = 0
         for a in iv:
             ci = set(range((self.po.M * a), (self.po.M * (a + 1))))
@@ -106,11 +114,10 @@ class Matrix:
         zr /= den
         mr /= den
         exp = (self.av - exp)
-        emr = len(conf ^ iv)
         for a in self.e.keys(): self.e[a] = {key:(value - 1) for key, value in self.e[a].items() if (value > 0)}
-        if (len(self.e.keys()) > self.po.C): self.e = {key:value for key, value in self.e.items() if (len(value.keys()) > 0)}
+        # if (len(self.e.keys()) > self.po.C): self.e = {key:value for key, value in self.e.items() if (len(value.keys()) > 0)}
         self.tp = sum(((len(self.e[a].keys()) * 2) + 1) for a in self.e.keys())
-        agency_str = f"\tBV: {bv}\tL:{self.ppcL}  R:{self.ppcR}" if (self.agency) else ""
+        agency_str = f"\tBV: {bv}\tL:{self.ppcL}  R:{self.ppcR}" if ((self.mi == 0) and (self.agency)) else ""
         # if (self.mi == 0): print(f"CY: {self.po.cycle}")
         print(f"M{self.mi}: EM: {(em * 100.0):.2f}%\tZR: {(zr * 100.0):.2f}%\tMR: {(mr * 100.0):.2f}%" + 
               f"\tEL: {len(self.e.keys())}   \tEX: {len(exp)}   \tER: {emr}   \tTP: {self.tp}" + agency_str)
