30415b0f94d144d3b1db4822b562211f605e4fa1
barrettwj <waylonbarrett6@gmail.com>
2024-03-26 06:47:36 -0500

    Add files via upload

--- Pattern_Classifier_V1.py
+++ Pattern_Classifier_V1.py
@@ -8,7 +8,6 @@ set_dim = 10
 #____________________________________________ARBITRARY TEST DATA________________________________
 archetype_set = [random.choice(set_values) for _ in range(set_dim)]
 max_diff = float(abs(max(set_values) - min(set_values)) * set_dim)
-enc_range = (set_dim * val_dim)
 num_test_sets = 100000
 test_data = []
 val_A = (score_dim - 1)
@@ -18,19 +17,19 @@ for i in range(num_test_sets):
     idx = val_A - round((float(sum([abs(a - b) for a, b in zip(archetype_set, rs)])) / max_diff) * norm_A)
     test_data.append([score_values[idx], rs.copy()])
 #________________________________________________EXPOSURE________________________________________
-learning_array = [0] * enc_range
+learning_array = [0] * (set_dim * val_dim)
 for v in test_data:
     score = (float(v[0] - 1) / norm_A)
     encoded_set = [((val_dim * i) + (a - 1)) for i, a in enumerate(v[1])]
-    learning_array = [(a + score) if i in encoded_set else (a - score) for i, a in enumerate(learning_array)]
-learning_array = [((a / float(num_test_sets)) + 1.0) for a in learning_array]
+    learning_array = [(b + score) if i in encoded_set else (b - score) for i, b in enumerate(learning_array)]
+learning_array = [(((a / float(num_test_sets)) + 1.0) / 2.0) for a in learning_array]
 #_________________________________________________RESULTS______________________________________
 #"""
 print(f"\n{archetype_set}  Archetype Set\n")
 for a in range(set_dim):
     td = {(b + 1):learning_array[((a * val_dim) + b)] for b in range(val_dim)}
     print(f"Digit at position {a} (Archetype {archetype_set[a]}) {('-' * 150)}\n")
-    for k, v in td.items(): print(f"{k}: {v:.5f}\t{('|' * round(v * 300.0))}")
+    for k, v in td.items(): print(f"{k}: {v:.5f}\t{('|' * round(v * 600.0))}")
     print()
 print(f"{archetype_set}  Archetype Set\n")
 num_rep_patterns = 5
@@ -40,9 +39,11 @@ for a in range(num_rep_patterns):
         tl = learning_array[(b * val_dim):((b + 1) * val_dim)]
         min_val = min(tl)
         diff = (max(tl) - min_val)
-        wheel = []
-        for i, c in enumerate(tl): wheel.extend([(i + 1) for _ in range(round(((c - min_val) / diff) * 1000.0))])
-        pattern.append(random.choice(wheel))
+        if (diff > 0):
+            wheel = []
+            for i, c in enumerate(tl): wheel.extend([(i + 1) for _ in range(round(((c - min_val) / diff) * 1000.0))])
+            pattern.append(random.choice(wheel))
+        else: pattern.append(0)
     idx = val_A - round((float(sum([abs(a - b) for a, b in zip(archetype_set, pattern)])) / max_diff) * norm_A)
     score_val = score_values[idx]
     print(f"{pattern}\t\tScore: {score_val}")
