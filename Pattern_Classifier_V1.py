14ee7d699f67dbeab3ad488360cfc0b7f8b786d2
barrettwj <waylonbarrett6@gmail.com>
2024-04-11 06:25:14 -0500

    Add files via upload

--- Pattern_Classifier_V1.py
+++ Pattern_Classifier_V1.py
@@ -8,7 +8,7 @@ score_dim = 3000
 score_values = [(a + 1) for a in range(score_dim)]
 set_dim = 10
 num_test_sets = 100000
-temp = 2.0
+temp = 1.0
 #____________________________________________ARBITRARY TEST DATA________________________________
 archetype_set = [random.choice(set_values) for _ in range(set_dim)]
 max_diff = float(abs(max(set_values) - min(set_values)) * set_dim)
@@ -22,7 +22,7 @@ for i in range(num_test_sets):
 #________________________________________________EXPOSURE________________________________________
 learning_array = [0] * (set_dim * val_dim)
 for v in test_data:
-    score = ((float(v[0] - 1) / norm_A) / 1000.0)
+    score = ((float(v[0] - 1) / norm_A) / 1000.0)#---------------------------------------------------------------HP
     encoded_set = [((val_dim * i) + (a - 1)) for i, a in enumerate(v[1])]
     learning_array = [(b + score) if i in encoded_set else (b - score) for i, b in enumerate(learning_array)]
 hm = (max(learning_array) / temp)
@@ -35,7 +35,7 @@ print(f"\n{archetype_set}  Archetype Set\n")
 for a in range(set_dim):
     td = {(b + 1):learning_array[((a * val_dim) + b)] for b in range(val_dim)}
     print(f"Digit at position {a} (Archetype {archetype_set[a]}) {('-' * 150)}\n")
-    for k, v in td.items(): print(f"{k}: {v:.5f}\t{('|' * round(v * 10000.0))}")
+    for k, v in td.items(): print(f"{k}: {v:.5f}\t{('|' * round(v * 8000.0))}")
     print()
 print(f"{archetype_set}  Archetype Set\n")
 num_rep_patterns = 5
