7d7a1380c659686a13b70327be6e72aa37834bf3
barrettwj <waylonbarrett6@gmail.com>
2024-05-27 05:46:19 -0500

    Add files via upload

--- Oracle_IDK_Mark_VI.py
+++ Oracle_IDK_Mark_VI.py
@@ -1,13 +1,14 @@
 import random
+import math
 class Oracle:
     def __init__(self):
         self.H = 3#-----------------------------------------------------------------------------------------HP
         self.M = 53#-53-------------------------------------------------------------------------------------HP
-        self.K = 97#-97-------------------------------------------------------------------------------------HP
-        self.adc_max = 37#----------------------------------------------------------------------------------HP
+        self.K = 167#-97------------------------------------------------------------------------------------HP
+        self.adc_max = 37#-37-------------------------------------------------------------------------------HP
         self.adc_min = round(self.adc_max * 0.70)
         ##############################################################################################################
-        self.tsp_dim_pct = 0.50#-0.30-----------------------------------------------------------------------HP
+        self.tsp_dim_pct = 0.40#-0.30-----------------------------------------------------------------------HP
         self.tsp_dim = round(self.K * self.tsp_dim_pct)
         ts_dim = 3#-----------------------------------------------------------------------------------------HP
         self.iv_mask = {a for a in range(self.K)}
@@ -42,8 +43,8 @@ class Matrix:
         self.em = self.em_prev = self.em_delta_abs = self.forget_period_ct = 0
         self.forget_period = 10#-----------------------------------------------------------------------------HP
     def update(self):
-        # fbv_raw = self.po.m[self.fbi].pv.copy()
-        fbv_raw = self.po.m[self.fbi].pv.copy() if self.fbi != 0 else set()
+        fbv_raw = self.po.m[self.fbi].pv.copy()
+        # fbv_raw = self.po.m[self.fbi].pv.copy() if self.fbi != 0 else set()
         fbv = {-(a + 1) for a in fbv_raw}
         """
         # if ((self.fbi == 0) and (random.randrange(1000000) < 900000)):
@@ -58,21 +59,21 @@ class Matrix:
         le = len(acts)
         self.ov = set()
         vi = dict()
-        alpha = 2#-musn't be too large!!!-------------------------------------------------------------------HP
+        # alpha = 4#-musn't be too large!!!--------------------------------------------------------------------------HP
         if le > 0:
-            mean = (sum(acts.values()) / le)
-            sigma = (sum(((v - mean) ** 2) for v in acts.values()) / le)
-            thresh = max(0, (mean - ((sigma ** 0.5) * alpha)))
-            rs = random.sample(range(le), le)
+            # mean = (sum(acts.values()) / le)
+            # sigma = (sum(((v - mean) ** 2) for v in acts.values()) / le)
+            # thresh = max(0, (mean - ((sigma ** 0.5) * alpha)))
+            # rs = random.sample(range(le), le)
             # aks = [a[2] for a in sorted([(v, rs.pop(), k) for k, v in acts.items() if (v <= thresh)])]#--------?????
-            aks = [a[2] for a in sorted([(v, rs.pop(), k) for k, v in acts.items() if (v < thresh)])]#---------?????
+            # aks = [a[2] for a in sorted([(v, rs.pop(), k) for k, v in acts.items() if (v < thresh)])]#---------?????
+            aks = [a[2] for a in self.softmax(acts)]
             for a in aks:
                 cli = (a // self.po.M)
                 if cli in vi:
                     vi[cli][1].add(a)
                     #########################################
-                    rel_v = (set(range((cli * self.po.M), ((cli + 1) * self.po.M))) & fbv_raw)
-                    rel_v = {-(b + 1) for b in rel_v}
+                    rel_v = {-(b + 1) for b in (set(range((cli * self.po.M), ((cli + 1) * self.po.M))) & fbv_raw)}
                     if a in self.e:
                         for b in rel_v: self.e[a][b] = random.randrange(self.po.adc_min, self.po.adc_max)
                     else: self.e[a] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in rel_v}
@@ -86,13 +87,13 @@ class Matrix:
         mr = (self.em / max(1, em_norm))
         ##############################################################################################################
         if self.ffi == -1:
-            # bv = {a for a in self.pv if ((a // self.po.M) in self.po.bv_mask)}
-            bv = self.pv.copy()
+            bv = {a for a in self.pv if ((a // self.po.M) in self.po.bv_mask)}
+            # bv = self.pv.copy()
             rs = random.sample(range(len(self.po.bv_map)), len(self.po.bv_map))
             bv_sorted = sorted([((len(k ^ bv) / max(1, (len(k) + len(bv)))), rs.pop(), k, v[0]) for k, v in self.po.bv_map.items()])
-            if len(bv_sorted) == 0: d = 1000000
+            if len(bv_sorted) == 0: d = 1
             else: d, r, bv_found, bv_idx = bv_sorted.pop(0)
-            if d > 0:#-------------------------------------------------------------------------------HP
+            if d > 0:#-------------------------------------------------------------------------------------------HP
                 # print(self.po.cy)
                 tl = list(self.po.iv_map.keys() - {v[0] for v in self.po.bv_map.values()})
                 bv_idx = random.choice(tl)
@@ -111,6 +112,7 @@ class Matrix:
             rs = random.sample(range(len(self.po.em_map)), len(self.po.em_map))
             em_sorted = sorted([(abs(k - self.em_delta_abs), rs.pop(), v) for k, v in self.po.em_map.items()])
             iv |= em_sorted.pop(0)[2]
+            ##########################################
         else: iv = self.po.m[self.ffi].ov.copy()
         ###########################################################################################################
         zr = 0
@@ -134,8 +136,7 @@ class Matrix:
                     cav = (ci - self.e.keys())
                 wi = random.choice(list(cav))
                 #########################################
-                rel_v = (ci & fbv_raw)
-                rel_v = {-(b + 1) for b in rel_v}
+                rel_v = {-(b + 1) for b in (ci & fbv_raw)}
                 if wi in self.e:
                     for b in rel_v: self.e[wi][b] = random.randrange(self.po.adc_min, self.po.adc_max)
                 else: self.e[wi] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in rel_v}
@@ -153,21 +154,22 @@ class Matrix:
         self.em_delta_abs = abs(self.em - self.em_prev)
         pv_ex = (pvc - pv_ack)
         ###########################################
-        """
         for a in pv_ex:
-            # if a in self.e:
-            #     for b in fbv: self.e[a][b] = random.randrange(self.po.adc_min, self.po.adc_max)
-            # else: self.e[a] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in fbv}
-            self.ov.add((a // self.po.M))
-        """
+            cli = (a // self.po.M)
+            rel_v = {-(b + 1) for b in (set(range((cli * self.po.M), ((cli + 1) * self.po.M))) & fbv_raw)}
+            if a in self.e:
+                for b in rel_v: self.e[a][b] = random.randrange(self.po.adc_min, self.po.adc_max)
+            else: self.e[a] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in rel_v}
+            self.ov.add(cli)
         ###########################################
         ##############################################################################################################
         self.forget_period_ct += 1
         if self.forget_period_ct == self.forget_period:
             tl = list(self.e.keys())
             for a in tl:
-                self.e[a] = {k:(v - 1) for k, v in self.e[a].items() if (v > 0)}
-                if not self.e[a]: del self.e[a]
+                if len(self.e[a]) > 100:#---------------------------------------------------------------------HP
+                    self.e[a] = {k:(v - 1) for k, v in self.e[a].items() if (v > 0)}
+                    if not self.e[a]: del self.e[a]
             self.forget_period_ct = 0
         ##############################################################################################################
         for a in self.e.keys():
@@ -180,5 +182,9 @@ class Matrix:
         print(f"M: {(self.ffi + 1)}  EM: {self.em:.2f}  EMDA: {self.em_delta_abs:.2f}" +
         f"  ES: {len(self.e):6d}  PV: {len(self.pv):4d}  ZR: {zr:.2f}  MR: {mr:.2f}  PVEX: {len(pv_ex):4d}" +
         f"  BVL: {len(self.po.bv_map):2d}" + bv_string)
+    def softmax(self, x):
+        sum_v = sum(math.exp(v) for v in x.values())
+        rs = random.sample(range(len(x)), len(x))
+        return sorted([((math.exp(v) / sum_v), rs.pop(), k) for k, v in x.items()])
 oracle = Oracle()
 oracle.update()
