d370a468049e77164a38688ebe139d6636768e85
barrettwj <waylonbarrett6@gmail.com>
2024-06-26 02:31:01 -0500

    Add files via upload

--- Oracle_IDK_Mark_VI.py
+++ Oracle_IDK_Mark_VI.py
@@ -1,19 +1,4 @@
 import random
-"""
-import sys
-import os
-# cookies found as "chats?bundleVersion=X.Y.Z" under "Network" tab of developer tools (right-click and Inspect in Bing chat box).
-# Copy everything after "Cookie:" Request Header
-with open('Copilot Cookies.txt', 'r') as file: os.environ["BING_COOKIES"] = file.read()
-import asyncio
-from sydney import SydneyClient
-from sydney.exceptions import (ConversationLimitException,
-                               NoConnectionException,
-                               NoResponseException,
-                               ThrottledRequestException,
-                               CaptchaChallengeException,
-                               )
-"""
 class Oracle:
     def __init__(self):
         self.H = 3#-----------------------------------------------------------------------------------------HP
@@ -43,56 +28,13 @@ class Oracle:
         em_interval = (1.0 / (em_num_vals - 1))
         self.em_map = {(a * em_interval):frozenset((em_start_idx + a + b)
                                                    for b in range(em_val_card)) for a in range(em_num_vals)}
-        """
         ##############################################################################################################
-        self.novel_chars_max = 500#-------------------------------------------------------------------------------HP
-        self.novel_chars = dict()
-        self.aff_response_start_idx = start_idx
-        self.aff_response_char_dim_max = (2 ** 14)#---------------------------------------------------------------HP
-        start_idx += (self.novel_chars_max * self.aff_response_char_dim_max)
-        ##############################################################################################################
-        self.aff_sugg_prompt_start_idx = start_idx
-        self.aff_sugg_prompt_char_dim_max = (2 ** 9)#-------------------------------------------------------------HP
-        start_idx += (self.novel_chars_max * self.aff_sugg_prompt_char_dim_max)
-        ##############################################################################################################
-        self.eff_prompt_start_idx = start_idx
-        self.eff_prompt_char_dim_max = (2 ** 11)#-----------------------------------------------------------------HP
-        start_idx += (self.novel_chars_max * self.eff_prompt_char_dim_max)
-        """
-        ##############################################################################################################
-        self.matrix_dim = start_idx
-        self.matrix_dim_offset = (self.matrix_dim * self.M)
+        self.matrix_dim_offset = (start_idx * self.M)
         self.m = [Matrix(self, a) for a in range(self.H)]
     def update(self):
-        # asyncio.run(self.sydney_interaction("What is the tallest building?"))
         while True:
             for a in self.m: a.update()
             self.cy += 1
-    """
-    async def sydney_interaction(self, prompt_in = "", set_style = "precise", reset_conv = False):
-        out = []
-        async with SydneyClient(style=set_style) as sydney:# style= "balanced", "precise", "creative"
-            if reset_conv: await sydney.reset_conversation()
-            try:
-                # print("Sydney: ", end="", flush=True)
-                response, suggested_responses = await sydney.ask(prompt_in, suggestions=True, citations=False)
-                # print(response, end="", flush=True)
-                # print("\n")
-                out.append(response)
-                for a in suggested_responses: out.append(a)
-                # if suggested_responses:
-                #     print("Suggested Responses: ", end="", flush=True)
-                #     print(suggested_responses, end="", flush=True)
-                # print("\n")
-            except ConversationLimitException:
-                await sydney.reset_conversation()
-                # asyncio.run(self.sydney_interaction(prompt_in))
-            except CaptchaChallengeException:
-                print("Captcha Exception!")
-                # asyncio.run(self.sydney_interaction(prompt_in))
-        print(out)
-        return out
-    """
 class Matrix:
     def __init__(self, po_in, mi_in):
         self.po = po_in
@@ -100,8 +42,7 @@ class Matrix:
         self.fbi = ((mi_in + 1) % self.po.H)
         self.MV = self.po.M
         self.iv = self.ov = self.av = set()
-        self.fbvm = dict()
-        self.e = dict()
+        self.e = self.fbvm = dict()
         self.em = self.em_prev = self.em_delta_abs = self.forget_period_ct = 0
         self.forget_period = 3#->=1----------------------------------------------------------------------------HP
         self.bv_list = []
@@ -121,10 +62,10 @@ class Matrix:
         le = len(acts)
         self.ov = set()
         vi = dict()
-        alpha = 1#-musn't be too large!!!--------------------------------------------------------------------------HP
+        alpha = 2#-musn't be too large!!!--------------------------------------------------------------------------HP
         if le > 0:
             mu = (sum(acts.values()) / le)
-            vari = (sum(((v - mu) ** 2) for v in acts.values()) / le)#----?????
+            vari = (sum(((v - mu) ** 2) for v in acts.values()) / le)
             sigma = (vari ** (1 / 2))
             thresh = max(0, (mu - (sigma * alpha)))
             elite = [(k, v) for k, v in acts.items() if (v <= thresh)]
@@ -188,18 +129,15 @@ class Matrix:
                         del self.e[tls[0][1]]
                         cav = (ci - self.e.keys())
                     wi = random.choice(list(cav))
-                #########################################
-                if a in self.fbvm:
-                    if wi in self.e: self.e[wi][self.fbvm[a]] = random.randrange(self.po.adc_min, self.po.adc_max)
-                    else: self.e[wi] = {self.fbvm[a]:random.randrange(self.po.adc_min, self.po.adc_max)}
                 self.ov.add(a)
-                #########################################
             else:
                 pv_ack |= ovl
                 wi = ovl.pop()
+            tv = cv.copy()
+            if a in self.fbvm: tv.add(self.fbvm[a])
             if wi in self.e:
-                for b in cv: self.e[wi][b] = random.randrange(self.po.adc_min, self.po.adc_max)
-            else: self.e[wi] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in cv}
+                for b in tv: self.e[wi][b] = random.randrange(self.po.adc_min, self.po.adc_max)
+            else: self.e[wi] = {b:random.randrange(self.po.adc_min, self.po.adc_max) for b in tv}
             self.av.add(wi)
         self.em /= max(1, (em_norm + len(self.iv)))
         zr /= max(1, len(self.iv))
@@ -212,7 +150,6 @@ class Matrix:
                 if a in self.e: self.e[a][self.fbvm[cli]] = random.randrange(self.po.adc_min, self.po.adc_max)
                 else: self.e[a] = {self.fbvm[cli]:random.randrange(self.po.adc_min, self.po.adc_max)}
             self.ov.add(cli)
-        ###########################################
         ##############################################################################################################
         self.forget_period_ct += 1
         if self.forget_period_ct == self.forget_period:
