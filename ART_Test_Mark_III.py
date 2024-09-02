9287d2c604e58ddf05e69a6dd24401d3e589bbf9
barrettwj <waylonbarrett6@gmail.com>
2024-09-02 00:38:40 -0500

    Add files via upload

--- ART_Test_Mark_III.py
+++ ART_Test_Mark_III.py
@@ -6,13 +6,12 @@ mem_max_dim = 200#--------------------------------------------------------------
 mem_min_dim = 3#---------------------------------------------------------------------------------------------HP
 mem_potential_idcs = set(range(mem_max_dim))
 max_val = 128#-----------------------------------------------------------------------------------------------HP
-# min_val = -(max_val - 1)#------------------------------------------------------------------------------------HP
-min_val = 0
+min_val = -(max_val - 1)#------------------------------------------------------------------------------------HP
+# min_val = 120
 abs_delta_range = (max_val - min_val)
-delta_thresh_pct = 0.005#-must be smaller than thresh????----------------------------------------------------HP
-delta_thresh = (abs_delta_range * delta_thresh_pct)
 max_delta = (abs_delta_range * N)
-thresh = 0.008#-0.009----------------------------------------------------------------------------------------HP
+blank_v = ([(min_val + (abs_delta_range / 2))] * N)
+thresh = 0.01#-0.008-----------------------------------------------------------------------------------------HP
 adc_max = 1000000000#----------------------------------------------------------------------------------------HP
 forget_period = 10
 forget_period_ct = 0
@@ -21,8 +20,14 @@ mem = dict()
 mk = mk_p = mkt = mkt_p = -1
 cy = exp = 0
 ################################################################################################################################
+ts_idx = 0
 ts_dim = 5#-------------------------------------------------------------------------------------------------HP
+##############################################################################
 ts = [[random.randint(min_val, max_val) for _ in range(N)] for _ in range(ts_dim)]
+##############################################################################
+# rv = [random.randint(min_val, max_val) for _ in range(N)]
+# ts = [rv.copy() for _ in range(ts_dim)]
+##############################################################################
 # ts = []
 # min_dist = 0.30
 # while (len(ts) < ts_dim):
@@ -34,13 +39,13 @@ ts = [[random.randint(min_val, max_val) for _ in range(N)] for _ in range(ts_dim
 #             flag = True
 #             break
 #     if not flag: ts.append(rv.copy())
-ts_idx = 0
 ################################################################################################################################
 act_v_prev = {k:0 for k in range(ts_dim)}
 act_v = dict()
-avg_diff = 0
-act_hist_depth = 7
+avg_dist = 0
+act_hist_depth = 13
 act_hist = []
+################################################################################################################################
 while True:
     exiv = ts[ts_idx].copy()
     ############################################################################################################################
@@ -51,7 +56,7 @@ while True:
     if (mem_dim_def > 0):
         for _ in range(mem_dim_def):
             avail_idcs = (mem_potential_idcs - mem.keys())
-            mem[random.choice(list(avail_idcs))] = [([0] * N), [0, 0], dict()]
+            mem[random.choice(list(avail_idcs))] = [blank_v.copy(), [0, 0], dict()]
     ############################################################################################################################
     mem_dim_exc = (len(mem) - mem_max_dim + 5)#---------------------------------------------------------------HP
     if (mem_dim_exc > 0):
@@ -59,7 +64,7 @@ while True:
             dC = [(k, v[1][1]) for k, v in mem.items()]
             rs = rsA.copy()
             dsC = sorted(dC, key = lambda x: (x[1], rs.pop(0)), reverse = True)
-            if (dsC[0][1] > 1): del mem[dsC[0][0]]
+            if (dsC[0][1] > 1): del mem[dsC[0][0]]#----------------------------------------------------------------HP
     ############################################################################################################################
     mk_prev = mk
     A = {k:sum((abs(x - y) / max_delta) for x, y in zip(exiv, v[0])) for k, v in mem.items()}
@@ -104,26 +109,23 @@ while True:
         for i, a in enumerate(exiv):
             delta_vector_A = (a - mem[mk][0][i])
             abs_delta_vector_A = abs(delta_vector_A)
-            delta_vector_A_pct_mag = (abs_delta_vector_A / abs_delta_range)
-            if (abs_delta_vector_A > 0.0001):#----------------------------------------------------------------------HP
-                mem[mk][0][i] += (delta_vector_A * 0.001)#----------------------------------------------------------HP
+            if (abs_delta_vector_A > 0.00001):#-0.01---------------------------------------------------------------------HP
+                mem[mk][0][i] += (delta_vector_A * 0.001)#-0.001---------------------------------------------------------HP
                 delta_vector_A = (a - mem[mk][0][i])
                 abs_delta_vector_A = abs(delta_vector_A)
-                delta_vector_A_pct_mag = (abs_delta_vector_A / abs_delta_range)
             for b in skip.keys():
                 delta_vector_B = (mem[b][0][i] - a)
                 abs_delta_vector_B = abs(delta_vector_B)
-                delta_vector_B_pct_mag = (abs_delta_vector_B / abs_delta_range)
-                diff = ((delta_vector_A_pct_mag + 0.01) - delta_vector_B_pct_mag)#------------------------------------HP
-                if (diff > 0.0001):#-----------------------------------------------------------------------------------HP
-                    signum_B = (delta_vector_B / max(1, abs_delta_vector_B))
-                    val_B = (mem[b][0][i] + (signum_B * diff * (skip_order[b] + 1) * 500.0))#------------------------HP
-                    if val_B < min_val: val_B = min_val
-                    if val_B > max_val: val_B = max_val
-                    mem[b][0][i] = val_B
+                if (abs_delta_vector_B > 0):
+                    diff = ((abs_delta_vector_A + 1) - abs_delta_vector_B)#-10----------------------------------------------HP
+                    if (diff > 0):
+                        signum_B = (delta_vector_B / max(1, abs_delta_vector_B))
+                        val_B = (mem[b][0][i] + (signum_B * diff))
+                        if val_B < min_val: val_B = min_val
+                        if val_B > max_val: val_B = max_val
+                        mem[b][0][i] = val_B
     #############################################################################################################################
     pes = " F"
-    # if (avg_diff == 1):
     for kA, vA in mem.items():
         td = vA[2].copy()
         for kB, vB in vA[2].items():
@@ -140,30 +142,31 @@ while True:
         mkt = mkt_p
         pes = "T  "
     mkt_p = -1
+    mk_p_set = set()
     for kA, vA in mem.items():
-        p_set = {kB for kB, vB in vA[2].items() if (vB == mkt)}
-        if (p_set):
-            mkt_p = p_set.pop()
-            for a in p_set: del mem[kA][2][a]#-----------------------------necessary?????----helpful?????
+        mkt_p_set = {kB for kB, vB in vA[2].items() if (vB == mkt)}
+        if (mkt_p_set):
+            mk_p_set.add(kA)
+            mkt_p = mkt_p_set.pop()
+            if (mkt_p_set):
+                #----------confidence/precision should be lowered proportional to the residual size of this set???!!!
+                for a in mkt_p_set: del mem[kA][2][a]#-----------------------------necessary?????----helpful?????
+    mk_p = mk_p_set.pop() if (mk_p_set) else -1
     #############################################################################################################################
     if ts_idx not in act_v: act_v[ts_idx] = mk
     else:
         act_hist.append((act_v.copy(), act_v_prev.copy()))
         act_v_prev = act_v.copy()
-        act_v = dict()
+        act_v = {ts_idx:mk}
         if (len(act_hist) == act_hist_depth):
-            avg_diff = 0
-            for i, a in enumerate(act_hist):
-                diff = (sum(1 for k, v in a[0].items() if (v == a[1][k])) / len(a[0]))
-                avg_diff += diff
-            avg_diff /= len(act_hist)
+            avg_dist = (sum((sum(1 for k, v in a[0].items() if (v == a[1][k])) / len(a[0])) for a in act_hist) / len(act_hist))
             act_hist = []
     #############################################################################################################################
     avg_tcd = sum(len(v[2]) for v in mem.values())
     avg_tcd /= len(mem)
     #############################################################################################################################
     print(f"ID: {str(ts_idx).rjust(3)}  MK: {str(mk).rjust(4)}  MV: {str(f'{mv:.4f}').rjust(5)}  ML: {str(len(mem)).rjust(4)}" + 
-        f"  DF: {str(f'{avg_diff:.2f}').rjust(4)}  SK: {str(len(skip)).rjust(3)}  PE: {pes.rjust(3)}  PV: {str(mkt_p).rjust(4)}" +
+        f"  DF: {str(f'{avg_dist:.2f}').rjust(4)}  SK: {str(len(skip)).rjust(3)}  PE: {pes.rjust(3)}  PV: {str(mk_p).rjust(4)}" +
         f"  RA: {str(rA).rjust(3)}  RB: {str(rB).rjust(3)}  RC: {str(rC).rjust(3)}  RD: {str(rD).rjust(3)}" +
         f"  ATCD: {str(f'{avg_tcd:.2f}').rjust(3)}  EXP: {str(exp).rjust(3)}")
     #############################################################################################################################
@@ -172,7 +175,7 @@ while True:
         dC = [(k, v[1][1]) for k, v in mem.items()]
         rs = rsA.copy()
         dsC = sorted(dC, key = lambda x: (x[1], rs.pop(0)), reverse = True)
-        if (dsC[0][1] > 1): del mem[dsC[0][0]]
+        if (dsC[0][1] > 100): del mem[dsC[0][0]]#-------------------------------------------------------------------------HP
         forget_period_ct = 0
     #############################################################################################################################
     ts_idx = ((ts_idx + 1) % len(ts))
