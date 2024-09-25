a1708756b6ebc419948e2240f7dc648bdd9ddb4c
barrettwj <waylonbarrett6@gmail.com>
2024-09-25 06:54:15 -0500

    Add files via upload

--- Oracle_Basic_Mark_III.py
+++ Oracle_Basic_Mark_III.py
@@ -1,37 +1,34 @@
 import random
-H = 7#-must be > 1 !!!-------------------------------------------------------------------------------HP
+H = 7#-----------------------------------------------------------------------------------------------HP
+K = 3#-----------------------------------------------------------------------------------------------HP
 i = 0
-mem = random.sample(list(range(-3, 4)), H)
-ext = 7
-ext_prev = -10
-# inf = -23#-----------this influence propagates down the hierarchy------------------------------------HP
-###########################################################################################################################
-ts_len = 3
-# ts = random.sample(range(-3, 4), ts_len)
+mem = random.sample(list(range(-K, K + 1)), H)
+ext = 1
+ext_prev = -1
+# inf = -1#-----------this influence propagates down the hierarchy------------------------------------HP
+ts_len = ((K * 2) + 1)#-must be > (K * 2)
 ts = list(range(ts_len))
 ts_idx = 0
+def wrap_value(val, minval, maxval): return (((val - minval) % (maxval - minval + 1)) + minval)
 while True:
-    #######################################################################################################################
-    fb = ext if i == (H - 1) else mem[i + 1]#---------Resonance???!!!----alter to add noise influence (creativity)
-    #######################################################################################################################    
+    fb = ext if i == (H - 1) else mem[i + 1]#------alter to add noise (creativity) influence
     if i == 0:
+        ##########################################################
+        tl = [ext]
+        tl.extend(mem)
+        w = 3
+        pcn = 2
+        f_str_list = [f"{x:>{w}.{pcn}f}" if isinstance(x, float) else f"{x:>{w}}" for x in tl]
+        f_str = " | ".join(f_str_list)
+        print(f_str)
+        ###########################################################
         ts_idx = (ts_idx + mem[i]) % ts_len
         # ext = ts[ts_idx]
-        ext = ext_prev - ext + ts[ts_idx]
-        # ext = ext_prev - ext + mem[i]
+        ext = ext_prev - ext + ts[ts_idx]#---this makes more sense than other strategies!!!!
+        # ext = ext_prev - ext + mem[i]#--------------------essence
         ext_prev = ext
         ff = ext
     else: ff = mem[i - 1]
-    ########################################################################################################################
-    mem[i] = ff - mem[i] + fb
-    ########################################################################################################################
-    tl = [ext]
-    tl.extend(mem)
-    w = 4
-    pcn = 2
-    f_str_list = [f"{x:>{w}.{pcn}f}" if isinstance(x, float) else f"{x:>{w}}" for x in tl]
-    f_str = " | ".join(f_str_list)
-    print(f_str)
-    ########################################################################################################################
+    # mem[i] = ff - mem[i] + fb#----------------------------essence
+    mem[i] = wrap_value((ff - mem[i] + fb), -K, K)
     i = (i + 1) % H
-    ########################################################################################################################
