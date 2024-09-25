689499a9c42e5f26ed8a5319fff8486a1871a56a
barrettwj <waylonbarrett6@gmail.com>
2024-09-25 04:22:39 -0500

    Add files via upload

--- Oracle_Basic_Mark_III.py
+++ Oracle_Basic_Mark_III.py
@@ -1,38 +1,37 @@
 import random
 H = 7#-must be > 1 !!!-------------------------------------------------------------------------------HP
-sample_range = list(range(-12, 13))
-mem = random.sample(sample_range, H)
-# mem = [2] * H
-mem_mirror = [-1000000] * H
+i = 0
+mem = random.sample(list(range(-3, 4)), H)
 ext = 7
 ext_prev = -10
-inf = -23#-----------this influence propagates down the hierarchy------------------------------------HP
-stasis_ss = 3#---------------------------------------------------------------------------------------HP
-stasis_samples = []
-i = 0
+# inf = -23#-----------this influence propagates down the hierarchy------------------------------------HP
+###########################################################################################################################
+ts_len = 3
+# ts = random.sample(range(-3, 4), ts_len)
+ts = list(range(ts_len))
+ts_idx = 0
 while True:
+    #######################################################################################################################
     fb = ext if i == (H - 1) else mem[i + 1]#---------Resonance???!!!----alter to add noise influence (creativity)
+    #######################################################################################################################    
     if i == 0:
-        ext = ext_prev - ext + mem[i]#-------------Agency???!!!---------this is much more stable!!!???
+        ts_idx = (ts_idx + mem[i]) % ts_len
+        # ext = ts[ts_idx]
+        ext = ext_prev - ext + ts[ts_idx]
+        # ext = ext_prev - ext + mem[i]
         ext_prev = ext
         ff = ext
     else: ff = mem[i - 1]
-    # fb = inf if i == (H - 1) else mem[i + 1]
-    # fb = ext if i == (H - 1) else mem[i + 1]#---------Resonance???!!!
+    ########################################################################################################################
     mem[i] = ff - mem[i] + fb
-    #############################################################
-    width = 6
-    precision = 2
-    formatted_list = [f"{x:>{width}.{precision}f}" if isinstance(x, float) else f"{x:>{width}}" for x in mem]
-    formatted_str = " | ".join(formatted_list)
-    print(formatted_str)
-    #############################################################
+    ########################################################################################################################
+    tl = [ext]
+    tl.extend(mem)
+    w = 4
+    pcn = 2
+    f_str_list = [f"{x:>{w}.{pcn}f}" if isinstance(x, float) else f"{x:>{w}}" for x in tl]
+    f_str = " | ".join(f_str_list)
+    print(f_str)
+    ########################################################################################################################
     i = (i + 1) % H
-    stasis_samples.append(sum([abs(x - y) for x, y in zip(mem, mem_mirror)]))
-    mem_mirror = mem.copy()
-    if len(stasis_samples) == stasis_ss:
-        avg_v = sum(stasis_samples) / stasis_ss
-        stasis_samples = []
-        if avg_v == 0: break#--------------------------------------------------------------------------HP
-# print(mem)
-print(ext)
+    ########################################################################################################################
--- Oracle_Basic_Mark_IV.py
+++ Oracle_Basic_Mark_IV.py
@@ -1,46 +1,49 @@
 import random
-H = 5#-must be > 1 !!!-------------------------------------------------------------------------------HP
-N = 3#-must be > 1 !!!-------------------------------------------------------------------------------HP
+H = 7#-----------------------------------------------------------------------------------------------HP
+N = 2#-must be > 1 !!!-------------------------------------------------------------------------------HP
 K = 3#-----------------------------------------------------------------------------------------------HP
-mem = [random.sample(range(-K, K + 1), N) for _ in range(H)]
-ext = [7] * N
-ext_prev = [-10] * N
-inf = [-2] * N#-----------this influence propagates down the hierarchy--------------------------------HP
-# stasis_ss = 23#-------------------------------------------------------------------------------------HP
-# stasis_samples = []
-# mem_mirror = [-100] * H
+M = ((K * 2) + 1)
 i = 0
-ts_len = 7#-must be > K * 2---------------------------------------------------------------------------HP
+mem = [random.sample(list(range(-K, K + 1)), N) for _ in range(H)]
+ext = random.sample(list(range(-K, K + 1)), N)
+ext_prev = random.sample(list(range(-K, K + 1)), N)
+inf = random.sample(list(range(-K, K + 1)), N)#---this influence propagates down the hierarchy-------HP
+##############################################################################################################################################
+ts_len = M#-must be >= M-----------------------------------------------------------------------------HP
 ts = list(range(ts_len))
+# ts = random.sample(list(range(-K, K + 1)), ts_len)
 ts_idx = 0
 while True:
-    # fb = ext if i == (H - 1) else mem[i + 1]#---------Resonance???!!!----alter to add noise influence (creativity)
-    fb = inf if i == (H - 1) else mem[i + 1]
+    ##########################################################################################################################################
+    fb = ext if i == (H - 1) else mem[i + 1]#---------Resonance???!!!----alter with added inf to add noise influence (creativity)???
+    # inf needs to intermittently have a non-zero value and then return to 0 !!!
+    ##########################################################################################################################################
     if i == 0:
-        ts_idx = ((ts_idx + ts_len + mem[i][0] + mem[i][2]) % ts_len)
-        ext = [mem[i][0], ts[ts_idx], mem[i][2]]
-        # ext = [xA + yA for xA, yA in zip([xB - yB for xB, yB in zip(ext_prev, ext)], mem[i])]# ext = ext_prev - ext + mem[i]#-----Agency???!!!
-        # ext_prev = ext
+        ##########################################################################
+        ts_idx = (ts_idx + mem[i][0]) % ts_len
+        # ts_idx += mem[i][0]
+        # if ts_idx < 0: ts_idx = 0
+        # if ts_idx > (ts_len - 1): ts_idx = (ts_len - 1)
+        ##########################################################################
+        ext = [mem[i][0], ts[ts_idx]]
+        # single value form:  ext = ext_prev - ext + mem[i]
+        # ext = [xA + yA for xA, yA in zip([xB - yB for xB, yB in zip(ext_prev, ext)], mem[i])]
+        # ext = [xA + yA for xA, yA in zip([xB - yB for xB, yB in zip(ext_prev, ext)], [mem[i][0], ts[ts_idx]])]
+        # # ext_prev = ext
         ff = ext
     else: ff = mem[i - 1]
-    mem[i] = [xA + yA for xA, yA in zip([xB - yB for xB, yB in zip(ff, mem[i])], fb)]# mem[i] = ff - mem[i] + fb
-    #############################################################
-    width = 4
-    precision = 2
-    tl = [ext.copy()]
-    tl.extend(mem.copy())
-    # formatted_list = [f"{x:>{width}.{precision}f}" if isinstance(x, float) else f"{x:>{width}}" for x in tl]
-    formatted_list = ["  ".join([f"{x:>{width}.{precision}f}" if isinstance(x, float) else f"{x:>{width}}" for x in a]) for a in tl]
-    formatted_str = "  | ".join(formatted_list)
-    print(formatted_str)
-    #############################################################
+    #############################################################################################################################################
+    # single value form:  mem[i] = ff - mem[i] + fb
+    mem[i] = [xA + yA for xA, yA in zip([xB - yB for xB, yB in zip(ff, mem[i])], fb)]
+    #############################################################################################################################################
+    if i == 0:
+        w = 6
+        pcn = 2
+        tl = [ext]
+        tl.extend(mem)
+        f_str_list = [" ".join([f"{x:>{w}.{pcn}f}" if isinstance(x, float) else f"{x:>{w}}" for x in a]) for a in tl]
+        f_str = " | ".join(f_str_list)
+        print(f_str)
+    #############################################################################################################################################
     i = (i + 1) % H
-    """
-    stasis_samples.append(sum([abs(x - y) for x, y in zip(mem, mem_mirror)]))
-    mem_mirror = mem.copy()
-    if len(stasis_samples) == stasis_ss:
-        avg_v = sum(stasis_samples) / stasis_ss
-        stasis_samples = []
-        if avg_v == 0: break#--------------------------------------------------------------------------HP
-    """
-print(ext)
+    #############################################################################################################################################
