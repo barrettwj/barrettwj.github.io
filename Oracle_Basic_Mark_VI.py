6dffc4cc2cf607a2073aeddb08e09b949b7211cf
barrettwj <waylonbarrett6@gmail.com>
2024-09-27 19:47:42 -0500

    Add files via upload

--- Oracle_Basic_Mark_VI.py
+++ Oracle_Basic_Mark_VI.py
@@ -1,21 +1,23 @@
 import random
-H = 7#------------------------------------------------------------------------------------------------HP
-K = 137#----------------------------------------------------------------------------------------------HP
+H = 5#------------------------------------------------------------------------------------------------HP
+K = 13717#--------------------------------------------------------------------------------------------HP
 nw = [random.randrange(-K, K) for _ in range(H + 1)]
 nwl = len(nw)
+w = len(str(K)) + 1
 ts_len = 5
 ts = list(range(ts_len))
 i = ts_idx = 0
 # def wrap_value(val, minval, maxval): return (((val - minval) % (maxval - minval + 1)) + minval)
 while True:
     fb = nw[(i + 1) % nwl]
+    no_pv = (nw[(i - 1) % nwl] - nw[i] + fb)
+    pv = (fb - nw[(i + 2) % nwl])
+    gradient_pct = 0.97#------------------------------------------------------------------------------HP
+    gradient = round((pv - no_pv) * gradient_pct)
     if i == 0:
-        print(" | ".join([f"{x:>{5}}" for x in nw]))
-        ts_idx = (ts_idx + fb) % ts_len
-        nw[i] = ts[ts_idx]
-    no_p_val = (nw[(i - 1) % nwl] - nw[i] + fb)
-    p_val = (fb - nw[(i + 2) % nwl])
-    gradient_pct = 0.07#------------------------------------------------------------------------------HP
-    gradient = round((p_val - no_p_val) * gradient_pct)
-    nw[i] = (no_p_val + gradient)
+        print(" | ".join([f"{x:>{w}}" for x in nw]))
+        ts_idx = (no_pv + gradient)#-------------Agency Enabled
+        # ts_idx = (ts_idx + 1)#-------------------Agency Disabled
+        nw[i] = ts[ts_idx % ts_len]
+    else: nw[i] = (no_pv + gradient)
     i = (i + 1) % H
