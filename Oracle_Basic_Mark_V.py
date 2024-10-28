e5e3c414d7ca583939d415acf3f89b34a5d85ed1
barrettwj <waylonbarrett6@gmail.com>
2024-10-28 05:06:39 -0500

    Add files via upload

--- Oracle_Basic_Mark_V.py
+++ Oracle_Basic_Mark_V.py
@@ -1,28 +1,29 @@
 import random
+import heapq
 H = 4
 N = 32
-K_pct = 0.37
+K_pct = 0.29
 K = round(N * K_pct)
-# eff_pct = 0.50
-# eff_dim = round(N * eff_pct)
-# eff_mask = set(range(eff_dim))
-# eff_card = round(len(eff_mask) * K_pct)
-# aff_mask = set(range(N)) - eff_mask
-# aff_card = K - eff_card
+################################################################
+eff_pct = 0.50
+eff_dim = round(N * eff_pct)
+eff_mask = set(range(eff_dim))
+eff_card = round(K * eff_pct)
+aff_mask = set(range(N)) - eff_mask
+aff_card = K - eff_card
+################################################################
 sample_range_list = list(range(N))
 mem = [set(random.sample(sample_range_list, K)) for _ in range(H)]
 #################################################################################
 ext = set(random.sample(sample_range_list, K))
 ext_prev = set()
 ts_dim = 37
-ts = [set(random.sample(sample_range_list, K)) for _ in range(ts_dim)]
-# ts = [set(random.sample(list(aff_mask), aff_card)) for _ in range(ts_dim)]
+ts = [set(random.sample(list(aff_mask), aff_card)) for _ in range(ts_dim)]
 rfb = set(random.sample(sample_range_list, K))
 ext_map = dict()
 bvrA = ts_dim - 1
 bvrB = (bvrA * 2) + 1
 bv_set = {-bvrA + a for a in range(bvrB)}
-rsA = random.sample(range(1000000), 1000000)
 #################################################################################
 i = em = ts_idx = 0
 def print_network():
@@ -42,38 +43,31 @@ while True:
         em /= len(mem)
         print_network()
         em = 0
+        ts_idx_inc = -1000000
         #########################################################################
         #"""
-        bv = mem[i].copy()
-        # bv = mem[i] & eff_mask
-        match = False
+        bv = mem[i] & eff_mask
         if ext_map:
-            d = [(len(k ^ bv), v) for k, v in ext_map.items()]
-            rs = rsA.copy()
-            d = sorted(d, key = lambda x: (x[0], rs.pop()))
-            if d[0][0] < 1:
-                ts_idx = (ts_idx + d[0][1]) % ts_dim
-                match = True
-        if not match:
-            rd = random.choice(list(bv_set))
-            ts_idx = (ts_idx + rd) % ts_dim
-            ext_map[frozenset(bv)] = rd
+            d = [(len(k ^ bv), k) for k in ext_map.keys()]
+            heapq.heapify(d)
+            d = sorted(d)
+            if d[0][0] < 1: ts_idx_inc = ext_map[d[0][1]]
+        if ts_idx_inc == -1000000:
+            ts_idx_inc = random.choice(list(bv_set))
+            ext_map[frozenset(bv)] = ts_idx_inc
         #"""
         #########################################################################
-        # ts_idx = (ts_idx + 1) % ts_dim
-        #########################################################################
-        res = ts[ts_idx].copy()
-        # res |= bv
-        ext = (ext_prev - res)
-        ext ^= mem[i]
-        ext_prev = ext.copy()
+        ts_idx = (ts_idx + ts_idx_inc) % ts_dim
+        # ext = ext_prev - (bv | ts[ts_idx])
+        ext = bv | ts[ts_idx]
+        # ext ^= mem[i]
+        # ext_prev = ext.copy()
         #########################################################################
         ff = ext.copy()
     else: ff = mem[i - 1].copy()
     #############################################################################
     ev = ff - mem[i]
     em += len(ev) / N
-    # em += len(ev) / (N * 2)
     mem[i] = ev ^ fb
     #############################################################################
     i = (i + 1) % H
