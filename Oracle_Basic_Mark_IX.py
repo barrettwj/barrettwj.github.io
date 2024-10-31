90b29b05d6a963531fb70bd33d3e50355778c3d2
barrettwj <waylonbarrett6@gmail.com>
2024-10-31 05:55:45 -0500

    Add files via upload

--- Oracle_Basic_Mark_IX.py
+++ Oracle_Basic_Mark_IX.py
@@ -4,19 +4,17 @@ class Oracle:
     def __init__(self):
         ###############################################################################################
         self.H = 6#---------------------------------------------------------------------------------------------HP
-        self.N = 64#--------------------------------------------------------------------------------------------HP
+        self.N = 64#-------------------------------------------------------------------------------------------HP
         self.N_range = range(self.N)
         self.sparsity = 0.02#-----------------------------------------------------------------------------------HP
         self.M = round(1 / self.sparsity)
         self.Q = self.N * self.M
-        self.K_pct = 0.51#--------------------------------------------------------------------------------------HP
+        self.K_pct = 0.53#--------------------------------------------------------------------------------------HP
         self.K = round(self.N * self.K_pct)
         ###############################################################################################
-        self.ts_dim = 3#----------------------------------------------------------------------------------------HP
-        self.ts_idx = 0
+        ts_dim = 3#---------------------------------------------------------------------------------------------HP
         self.ts = [{random.choice(range(a * self.M, (a + 1) * self.M))
-                    for a in random.sample(self.N_range, self.K)} for _ in range(self.ts_dim)]
-        self.ts_idx_inc_set = {a for a in range(self.ts_dim)}
+                    for a in random.sample(self.N_range, self.K)} for _ in range(ts_dim)]
         ###############################################################################################
         self.ext_map = dict()
         self.ext_map_adc_max = 1000#----------------------------------------------------------------------------HP
@@ -42,7 +40,8 @@ class Matrix:
         ###############################################################################################
         self.adc_max = 10000000#-------------------------------------------------------------------------------HP
         self.ts_idx_inc = -1
-        self.em = self.em_prev = self.em_delta = 0
+        self.ts_idx_inc_set = {a for a in range(len(self.po.ts))}
+        self.em = self.em_prev = self.em_delta = self.ts_idx = 0
     def update(self):
         ###############################################################################################
         if self.ffi == -1:
@@ -57,16 +56,17 @@ class Matrix:
                     self.ts_idx_inc = self.po.ext_map[d[0][1]][0]
                     self.po.ext_map[d[0][1]][1] = 0
             if self.ts_idx_inc == -1:
-                self.ts_idx_inc = random.choice(list(self.po.ts_idx_inc_set))
+                self.ts_idx_inc = random.choice(list(self.ts_idx_inc_set))
                 self.po.ext_map[frozenset(bv)] = [self.ts_idx_inc, 0]
-            if len(self.po.ext_map) > 300:#----------------------------------------------------------------------------HP
+            if len(self.po.ext_map) > 300:#----------------------------------------------------------------------HP
                 for v in self.po.ext_map.values():
                     if v[1] < self.po.ext_map_adc_max: v[1] += 1
-                self.po.ext_map = {k:v for k, v in self.po.ext_map.items() if v[1] < self.po.ext_map_adc_max - 10}#----HP
-            self.po.ts_idx = (self.po.ts_idx + self.ts_idx_inc) % self.po.ts_dim
+                self.po.ext_map = {k:v for k, v in self.po.ext_map.items()
+                                   if v[1] < self.po.ext_map_adc_max - 10}#--------------------------------------HP
+            self.ts_idx = (self.ts_idx + self.ts_idx_inc) % len(self.po.ts)
             # ff_prev_cl = self.convert(ff)
-            # ff = ext_prev_cl - self.po.ts[self.po.ts_idx]
-            ff = self.po.ts[self.po.ts_idx].copy()
+            # ff = ext_prev_cl - self.po.ts[self.ts_idx]
+            ff = self.po.ts[self.ts_idx].copy()
         else: ff = self.po.m[self.ffi].pv.copy()
         ###############################################################################################
         ff_cl = self.convert(ff)
@@ -103,21 +103,22 @@ class Matrix:
                     for b in self.cv: self.e[wi][b] = 0
                 else: self.e[wi] = {b:0 for b in self.cv}
         ###############################################################################################
+        self.cv = self.pv.copy()
+        ###############################################################################################
         fb = self.fb_alt.copy() if self.fbi == 0 else self.po.m[self.fbi].pv.copy()
         fb_cl = self.convert(fb)
         er ^= fb_cl
-        er_el = set()
+        mr = 0
+        self.pv = set()
         for a in er:
             ci = set(range(a * self.po.M, (a + 1) * self.po.M))
-            if a in ff_cl: tv = ff & ci
-            if a in fb_cl: tv = fb & ci
-            if len(tv) == 1: er_el.add(tv.pop())
+            if a in ff_cl: tvA = ff & ci
+            if a in fb_cl: tvA = fb & ci
+            if len(tvA) == 1: self.pv.add(tvA.pop())
+            else: mr += 1
         ###############################################################################################
-        self.cv = self.pv.copy()#----------???
-        ###############################################################################################
-        self.pv = er_el.copy()
-        remn = set(self.po.N_range) - self.convert(er_el)
-        thresh_B = 0.90#-----------------------------------------------------------------------------------------------HP
+        remn = set(self.po.N_range) - self.convert(self.pv)
+        thresh_B = 0.99#----------------------------------------------------------------------------------------HP
         maxval = 1
         avgd = 0
         avgd_ct = 0
@@ -130,26 +131,28 @@ class Matrix:
                 avgd_ct += 1
                 minval = -(len(b) - 1)
                 norm = maxval - minval
-                c = {k:(len(self.e[k].keys() & self.cv) / max(1, len(self.e[k].keys() | self.cv))) for k in b}#-Jaccard similarity
+                c = {k:(len(self.e[k].keys() & self.cv) / max(1, len(self.e[k].keys() | self.cv))) for k in b}#-Jaccard Index
+                # c = {k:(1 - (len(self.e[k].keys() ^ self.cv) / self.po.Q)) for k in b}#-Hamming Distance----issues???
                 d = [((((vA - sum(vB for kB, vB in c.items() if (kB != kA))) - minval) / norm), kA) for kA, vA in c.items()]
                 heapq.heapify(d)
                 d = sorted(d, key = lambda x: x[0], reverse = True)
                 avgs += d[0][0]
                 avgs_ct += 1
                 if d[0][0] >= thresh_B: self.pv.add(d[0][1])
-        avgd /= max(1, avgd_ct)
-        avgs /= max(1, avgs_ct)
+        avgd = avgd / avgd_ct if avgd_ct > 0 else -1
+        avgs = avgs / avgs_ct if avgs_ct > 0 else -1
         ###############################################################################################
-        # if self.em < 0.0001 and self.em_delta < 0.0001 and len(self.po.bv_map) < self.po.bv_map_max:#---------------------------HP
+        # if self.em < 0.0001 and self.em_delta < 0.0001 and len(self.po.bv_map) < self.po.bv_map_max:#----------HP
         ###############################################################################################
         tl = [f"M{str(self.ffi + 1).rjust(1)}"]
         tl.append(f"EM: {f'{self.em:.2f}'.rjust(4)}")
         tl.append(f"PV: {str(len(self.pv)).rjust(3)}")
         tl.append(f"EL: {str(len(self.e)).rjust(8)}")
         # tl.append(f"EMDA: {f'{self.em_delta_avg:.3f}'.rjust(6)}")
-        tl.append(f"AM: {f'{avgs:.2f}'.rjust(4)}")
+        tl.append(f"AM: {f'{avgs:.2f}'.rjust(5)}")
         tl.append(f"ED: {str(round(avgd)).rjust(3)}")
         tl.append(f"ZR: {str(zr).rjust(4)}")
+        tl.append(f"MR: {str(mr).rjust(4)}")
         tl.append(f"EXL: {str(len(self.po.ext_map)).rjust(3)}")
         if self.ffi == -1: tl.append(f"EXI: {str(self.ts_idx_inc).rjust(3)}")
         print(" | ".join(tl))
