8f5b9919541f774cd541f3d229dc3e30c0e77d77
barrettwj <waylonbarrett6@gmail.com>
2024-11-05 08:54:42 -0600

    Add files via upload

--- Oracle_Basic_Mark_V.py
+++ Oracle_Basic_Mark_V.py
@@ -1,31 +1,32 @@
 import random
 import heapq
-H = 6
-N = 24
+H = 5
+N = 28#-24
 D = N * 2
+# D = N
 N_range = range(N)
+D_range = range(D)
 N_inv_list = [-N + a for a in N_range]
-K_pct = 0.12
+K_pct = 0.11#-0.12
 K = round(N * K_pct)
 #################################################################################
-oracle = [set(random.sample(N_range, K)) for _ in range(H)]
-evs = [set(random.sample(N_range, K)) for _ in range(H)]
+oracle = [set(random.sample(D_range, K * 2)) for _ in range(H)]
+evs = oracle.copy()
 #################################################################################
+ev = set()
 ext = set()
-ts_dim = 67
-ts = [set(random.sample(N_range, K)) for _ in range(ts_dim)]
 # fb_alt = set(random.sample(N_range, K))
 fb_alt = set()
 ext_map_adc_max = 200
 ext_map = dict()
-bv_set = {a for a in range(ts_dim)}
 trn_map_max_dim = 500
 trn_map = dict()
 #################################################################################
 symchar = '\u25AA'
-i = em = ts_idx = ts_idx_inc = em_prev = em_delta = 0
+i = i_prev = em = em_prev = em_delta = ts_idx = 0
+ts_res = set()
 def print_network():
-    fl = [str(ts_idx_inc).rjust(2)]
+    fl = [str(ts_idx).rjust(2)]
     tl = [ext.copy()]
     tl.extend(oracle)
     # tl.extend(evs)
@@ -38,42 +39,41 @@ def print_network():
 #################################################################################
 while True:
     #############################################################################
-    # fb_alt = ext.copy()
+    fb_alt = ext.copy()
     fb = fb_alt.copy() if i == H - 1 else oracle[i + 1].copy()
     #############################################################################
+    oracle[i_prev] = ev ^ fb
     if i == 0:
         em /= H
         em_delta = em - em_prev
         print_network()
         em_prev = em
         em = 0
-        ts_idx_inc = -1000000
         #########################################################################
+        pre = ts_res.copy()
         bv = oracle[i].copy()
-        if ext_map:
-            d = [(len(k ^ bv), k) for k in ext_map.keys()]
+        d = [(len(k ^ bv), k) for k in ext_map.keys() if len(k ^ bv) < 1]
+        if d:
             heapq.heapify(d)
             d = sorted(d, key = lambda x: x[0])
-            if d[0][0] < 1:
-                ts_idx_inc = ext_map[d[0][1]][0]
-                ext_map[d[0][1]][1] = 0
-        if ts_idx_inc == -1000000:
-            ts_idx_inc = random.choice(list(bv_set))
-            ext_map[frozenset(bv)] = [ts_idx_inc, 0]
+            ts_res = ext_map[d[0][1]][0].copy()
+            ext_map[d[0][1]][1] = 0
+        else:
+            ts_res = set(random.sample(N_range, K))
+            ext_map[frozenset(bv)] = [ts_res.copy(), 0]
         for v in ext_map.values():
             if v[1] < ext_map_adc_max: v[1] += 1
-        ext_map = {k:v for k, v in ext_map.items() if v[1] < ext_map_adc_max - 10}
+        ext_map = {k:v for k, v in ext_map.items() if v[1] < 10}
         #########################################################################
-        ext = {-(a + 1) for a in ts[ts_idx]}
-        ts_idx = (ts_idx + ts_idx_inc) % ts_dim
-        ext |= ts[ts_idx]
+        ext = ts_res.copy()
+        ext |= {-(a + 1) for a in pre}
         #########################################################################
         ff = ext.copy()
     else: ff = oracle[i - 1].copy()
     #############################################################################
     ev = ff - oracle[i]
     evs[i] = ev.copy()
-    oracle[i] = ev ^ fb
     em += len(ev) / D
     #############################################################################
+    i_prev = i
     i = (i + 1) % H
