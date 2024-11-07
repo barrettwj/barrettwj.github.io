2c3968e8143acaef97e24e8aa62bdaac0ee50cc8
barrettwj <waylonbarrett6@gmail.com>
2024-11-07 09:11:57 -0600

    Add files via upload

--- Oracle_Basic_Mark_IX.py
+++ Oracle_Basic_Mark_IX.py
@@ -4,8 +4,10 @@ class Oracle:
     def __init__(self):
         ###############################################################################################
         self.H = 6#---------------------------------------------------------------------------------------------HP
-        self.N = 67#--------------------------------------------------------------------------------------------HP
+        self.N = 47#--------------------------------------------------------------------------------------------HP
         self.N_range = range(self.N)
+        self.D = self.N * 2
+        self.D_range = [-self.N + a for a in range(self.D)]
         self.M = 47#--------------------------------------------------------------------------------------------HP
         self.Q = self.N * self.M
         self.K_pct = 0.53#--------------------------------------------------------------------------------------HP
@@ -46,6 +48,7 @@ class Matrix:
         self.fbi = (mi_in + 1) % self.po.H
         ###############################################################################################
         self.e = dict()
+        self.bv_prev = set()
         self.pv = set()
         self.cv = set()
         self.er = set()
@@ -55,8 +58,8 @@ class Matrix:
         self.fb = set()
         self.fb_cl = set()
         self.pv_cl = set()
-        # self.fb_alt = set(random.sample(self.po.N_range, self.po.K))
         self.fb_alt = set()
+        # self.fb_alt = set(random.sample(self.po.N_range, self.po.K))
         self.adc_max = 5000
         ###############################################################################################
         self.ts_idx_set = {a for a in range(len(self.po.ts))}
@@ -66,50 +69,14 @@ class Matrix:
         self.fb = self.fb_alt.copy() if self.fbi == 0 else self.po.m[self.fbi].pv.copy()
         self.fb_cl = self.convert(self.fb)
         ###############################################################################################
-        self.er ^= self.fb_cl
-        ###############################################################################################
-        self.pv = set()
-        for a in self.er:
-            ci = set(range(a * self.po.M, (a + 1) * self.po.M))
-            tvA = (self.ff & ci) | (self.fb & ci)
-            if len(tvA) == 1: self.pv.add(tvA.pop())
-        ###############################################################################################
-        for vA in self.e.values():
-            for vB in vA.values():
-                if vB < self.adc_max: vB += 1
-        ###############################################################################################
-        remn = set(self.po.N_range) - self.convert(self.pv)
-        thresh_A = 0.95#-------------------------------------------------------------------------------------HP
-        maxval = 1
-        avgd = 0
-        avgd_ct = 0
-        avgs = 0
-        avgs_ct = 0
-        for a in remn:
-            b = [k for k in self.e.keys() if (k // self.po.M) == a]
-            if b:
-                avgd += len(b)
-                avgd_ct += 1
-                c = {k:(len(self.e[k].keys() & self.cv) / max(1, len(self.e[k].keys() | self.cv))) for k in b}#-Jaccard Index
-                # c = {k:(1 - (len(self.e[k].keys() ^ self.cv) / max(1, len(self.e[k].keys() | self.cv)))) for k in b}#-Hamming Distance
-                if len(c) > 1:
-                    minval = -(len(c) - 1)
-                    norm = maxval - minval
-                    d = [((((vA - sum(vB for kB, vB in c.items() if (kB != kA))) - minval) / norm), kA) for kA, vA in c.items()]
-                    heapq.heapify(d)
-                    d = sorted(d, key = lambda x: x[0], reverse = True)
-                else: d = [(v, k) for k, v in c.items()]
-                avgs += sum(y[0] for y in d) / max(1, len(d))
-                # avgs += sum(c.values()) / len(c)
-                avgs_ct += 1
-                if d[0][0] >= thresh_A: self.pv.add(d[0][1])
-        ###############################################################################################
         ts_idx = -1
         if self.ffi == -1:
             for v in self.po.ext_map.values():
                 if v[1] < self.po.ext_map_adc_max: v[1] += 1
             self.po.ext_map = {k:v for k, v in self.po.ext_map.items() if v[1] < 23}#----------------------HP
-            bv = self.pv.copy()
+            # bv = self.pv.copy()
+            bv = self.bv_prev.copy()
+            self.bv_prev = self.pv.copy()
             d = [(len(k ^ bv), k) for k in self.po.ext_map.keys() if (len(k ^ bv) < 1)]#------------------HP
             if d:
                 if len(d) > 1:
@@ -120,7 +87,9 @@ class Matrix:
             if ts_idx == -1:
                 ts_idx = random.choice(list(self.ts_idx_set))
                 self.po.ext_map[frozenset(bv)] = [ts_idx, 0]
-            self.ff = self.po.ts[ts_idx].copy()
+            self.ff = {-(a + 1) for a in self.ff if (a // self.po.M) in self.po.N_range}
+            self.ff |= self.po.ts[ts_idx]
+            # self.ff = self.po.ts[ts_idx].copy()
         else: self.ff = self.po.m[self.ffi].pv.copy()
         ###############################################################################################
         self.ff_cl = self.convert(self.ff)
@@ -155,17 +124,56 @@ class Matrix:
                     for b in self.cv: self.e[wi][b] = 0
                 else: self.e[wi] = {b:0 for b in self.cv}
         ###############################################################################################
-        self.em = len(self.er) / self.po.N
+        # self.em = len(self.er) / self.po.N
+        self.em = len(self.er) / self.po.D
         self.em_delta = self.em - self.em_prev
         self.em_prev = self.em
         ###############################################################################################
-        self.cv = self.av | {a + self.po.Q for a in self.pv}#----best???!!!
-        # self.cv = self.av.copy()
-        # self.cv = self.pv.copy()                    
+        # self.cv = self.av | {a + self.po.Q for a in self.pv}#----best???!!!
+        self.cv = self.av.copy()
+        # self.cv = self.pv.copy()
+        ###############################################################################################
+        self.er ^= self.fb_cl
+        ###############################################################################################
+        self.pv = set()
+        for a in self.er:
+            ci = set(range(a * self.po.M, (a + 1) * self.po.M))
+            tvA = (self.ff & ci) | (self.fb & ci)
+            if len(tvA) == 1: self.pv.add(tvA.pop())
+        ###############################################################################################
+        for vA in self.e.values():
+            for vB in vA.values():
+                if vB < self.adc_max: vB += 1
+        ###############################################################################################
+        # remn = set(self.po.N_range) - self.convert(self.pv)
+        remn = set(self.po.D_range) - self.convert(self.pv)
+        thresh_A = 0.90#-------------------------------------------------------------------------------------HP
+        maxval = 1
+        avgd = 0
+        avgd_ct = 0
+        avgs = 0
+        avgs_ct = 0
+        for a in remn:
+            b = [k for k in self.e.keys() if (k // self.po.M) == a]
+            if b:
+                avgd += len(b)
+                avgd_ct += 1
+                c = {k:(len(self.e[k].keys() & self.cv) / max(1, len(self.e[k].keys() | self.cv))) for k in b}#-Jaccard Index
+                if len(c) > 1:
+                    minval = -(len(c) - 1)
+                    norm = maxval - minval
+                    d = [((((vA - sum(vB for kB, vB in c.items() if (kB != kA))) - minval) / norm), kA) for kA, vA in c.items()]
+                    heapq.heapify(d)
+                    d = sorted(d, key = lambda x: x[0], reverse = True)
+                else: d = [(v, k) for k, v in c.items()]
+                avgs += sum(y[0] for y in d) / max(1, len(d))
+                # avgs += sum(c.values()) / len(c)
+                avgs_ct += 1
+                if d[0][0] >= thresh_A: self.pv.add(d[0][1])                    
         ###############################################################################################
         tl = [f"M{str(self.ffi + 1).rjust(1)}"]
         tl.append(f"EM: {f'{self.em:.2f}'.rjust(4)}")
-        tl.append(f"PV: {str(len(self.pv)).rjust(3)}")
+        tl.append(f"PV: {str(len(self.pv)).rjust(4)}")
         tl.append(f"EL: {str(len(self.e)).rjust(6)}")
         if avgs_ct > 0: tl.append(f"AM: {f'{avgs / avgs_ct:.2f}'.rjust(4)}")
         else: tl.append(f"AM: ----")
@@ -175,7 +183,7 @@ class Matrix:
         tl.append(f"ZR: {f'{zr:.2f}'.rjust(4)}")
         tl.append(f"EXL: {str(len(self.po.ext_map)).rjust(3)}")
         if self.ffi == -1: tl.append(f"EXI: {str(ts_idx).rjust(3)}")
-        print(" | ".join(tl))     
+        print(" | ".join(tl))
         ###############################################################################################
     def convert(self, v_in):
         out = set()
--- Oracle_Basic_Mark_V.py
+++ Oracle_Basic_Mark_V.py
@@ -3,46 +3,42 @@ import heapq
 H = 5
 N = 28#-24
 D = N * 2
-# D = N
 N_range = range(N)
 D_range = range(D)
 N_inv_list = [-N + a for a in N_range]
-K_pct = 0.11#-0.12
+K_pct = 0.47#-0.12
 K = round(N * K_pct)
 #################################################################################
-oracle = [set(random.sample(D_range, K * 2)) for _ in range(H)]
+oracle = [set() for _ in range(H)]
 evs = oracle.copy()
 #################################################################################
-ev = set()
 ext = set()
-# fb_alt = set(random.sample(N_range, K))
+bv_prev = set()
 fb_alt = set()
-ext_map_adc_max = 200
+# fb_alt = set(random.sample(N_range, K))
+ext_map_adc_max = 11
 ext_map = dict()
-trn_map_max_dim = 500
-trn_map = dict()
+ts_res = set()
+idx_map = dict()
 #################################################################################
 symchar = '\u25AA'
-i = i_prev = em = em_prev = em_delta = ts_idx = 0
-ts_res = set()
+i = em = em_prev = em_delta = ts_idx = 0
 def print_network():
-    fl = [str(ts_idx).rjust(2)]
+    fl = [str(ts_idx).rjust(3)]
     tl = [ext.copy()]
     tl.extend(oracle)
     # tl.extend(evs)
     fl.extend(["".join([f"{symchar if x in y else ' '}" for x in N_range]) for y in tl])
     # fl.extend(["".join([f"{symchar if x in y else ' '}" for x in N_inv_list]) for y in tl])
     fl.append(f"EM: {f'{em:.3f}'.rjust(5)}")
-    # fl.append(f"ED: {f'{em_delta:.3f}'.rjust(6)}")
-    fl.append(f"EX: {str(len(ext_map)).rjust(4)}")
+    fl.append(f"EX: {str(len(ext_map)).rjust(3)}")
     print("|".join(fl))
 #################################################################################
 while True:
     #############################################################################
-    fb_alt = ext.copy()
+    # fb_alt = ext.copy()
     fb = fb_alt.copy() if i == H - 1 else oracle[i + 1].copy()
     #############################################################################
-    oracle[i_prev] = ev ^ fb
     if i == 0:
         em /= H
         em_delta = em - em_prev
@@ -50,23 +46,31 @@ while True:
         em_prev = em
         em = 0
         #########################################################################
-        pre = ts_res.copy()
-        bv = oracle[i].copy()
+        # bv = oracle[i].copy()
+        bv = bv_prev.copy()
+        bv_prev = oracle[i].copy()
         d = [(len(k ^ bv), k) for k in ext_map.keys() if len(k ^ bv) < 1]
         if d:
             heapq.heapify(d)
             d = sorted(d, key = lambda x: x[0])
             ts_res = ext_map[d[0][1]][0].copy()
+            ts_idx = idx_map[frozenset(ts_res)]
             ext_map[d[0][1]][1] = 0
         else:
             ts_res = set(random.sample(N_range, K))
+            while frozenset(ts_res) in idx_map.keys(): ts_res = set(random.sample(N_range, K))
             ext_map[frozenset(bv)] = [ts_res.copy(), 0]
+            ts_idx = 0
+            while ts_idx in idx_map.values(): ts_idx += 1
+            idx_map[frozenset(ts_res)] = ts_idx
         for v in ext_map.values():
             if v[1] < ext_map_adc_max: v[1] += 1
-        ext_map = {k:v for k, v in ext_map.items() if v[1] < 10}
+        ext_map = {k:v for k, v in ext_map.items() if v[1] < ext_map_adc_max}
+        vs = [a[0] for a in ext_map.values()]
+        idx_map = {k:v for k, v in idx_map.items() if set(k) in vs}
         #########################################################################
-        ext = ts_res.copy()
-        ext |= {-(a + 1) for a in pre}
+        ext = {-(a + 1) for a in ext if a in N_range}
+        ext |= ts_res
         #########################################################################
         ff = ext.copy()
     else: ff = oracle[i - 1].copy()
@@ -74,6 +78,6 @@ while True:
     ev = ff - oracle[i]
     evs[i] = ev.copy()
     em += len(ev) / D
+    oracle[i] = ev ^ fb
     #############################################################################
-    i_prev = i
     i = (i + 1) % H
